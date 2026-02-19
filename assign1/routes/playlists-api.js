c2fdbd81a648aa981464e2a21de05a628175dcf9
Mjohn572 <mjohn572@mtroyal.ca>
2026-02-18 22:38:23 -0700

    modified:   assign1/routes/playlists-api.js

--- assign1/routes/playlists-api.js
+++ assign1/routes/playlists-api.js
@@ -2,7 +2,7 @@ export function playlistRoutes(app, db) {
 
     // Grabs all playlists with extra fields
     app.get('/api/playlists', (req, res) => {
-        const playlist = db.prepare(`
+        const playlists = db.prepare(`
             SELECT p.playlist_id, 
             s.song_id,
             s.title,
@@ -13,10 +13,9 @@ export function playlistRoutes(app, db) {
             INNER JOIN songs as s ON p.song_id = s.song_id
             INNER JOIN artists as a ON s.artist_id = a.artist_id
             INNER JOIN genres as g ON s.genre_id = g.genre_id
-            WHERE p.playlist_id = ?
             `).all();
-        if(!playlist.length) return res.status(404).json({ error: `No Playlist Found` });
-        res.json(playlist);
+        if(!playlists.length) return res.status(404).json({ error: `No Playlists Found` });
+        res.json(playlists);
     })
     // Grabs playlists with given ID
     app.get('/api/playlists/:playlist_id', (req, res) => {
