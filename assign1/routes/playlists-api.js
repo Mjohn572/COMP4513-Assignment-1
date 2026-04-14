9d45f83aaeb3550bdc97b6cd3fca7c7a13096517
Mjohn572 <mjohn572@mtroyal.ca>
2026-04-13 21:16:26 -0600

    Modified Playlists to fit Assignment 2 criteria

--- assign1/routes/playlists-api.js
+++ assign1/routes/playlists-api.js
@@ -2,7 +2,7 @@ export function playlistRoutes(app, db) {
 
     // Grabs all playlists with extra fields
     app.get('/api/playlists', (req, res) => {
-        const playlists = db.prepare(`
+        const rows = db.prepare(`
             SELECT p.playlist_id, 
             s.song_id,
             s.title,
@@ -15,8 +15,34 @@ export function playlistRoutes(app, db) {
             INNER JOIN genres as g ON s.genre_id = g.genre_id
             `).all();
         if(!playlists.length) return res.status(404).json({ error: `No Playlists Found` });
+        // Group rows by playlist_id
+        const grouped = rows.reduce((acc, row) => {
+            // If this playlist_id hasn't been seen yet, create it
+            if (!acc[row.playlist_id]) {
+                acc[row.playlist_id] = {
+                    playlist_id: row.playlist_id,
+                    songs: []
+                }
+            }
+
+            // Push the song into the matching playlist's songs array
+            acc[row.playlist_id].songs.push({
+                song_id: row.song_id,
+                title: row.title,
+                artist_name: row.artist_name,
+                genre_name: row.genre_name,
+                year: row.year,
+            })
+
+            return acc;
+        }, {})
+
+        // Convert the object into an array
+        const playlists = Object.values(grouped);
+
         res.json(playlists);
     })
+    
     // Grabs playlists with given ID
     app.get('/api/playlists/:playlist_id', (req, res) => {
         const playlist = db.prepare(`
